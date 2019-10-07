e72acb1630be6e5593fb3f7465492aac7498b37a
cVidalSP <kainan.vidal@gmail.com>
2019-10-06 23:23:10 -0300

    Conclusao do app

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,38 @@
 const express = require('express');
-const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
+
+
+const routes = require('./routes');
 
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 // Conectar com a base do mongodbAtlas
 mongoose.connect('mongodb+srv://vidal:vidal@omnistack-cfebw.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser : true,
     useUnifiedTopology : true,
 });
+
+const connectedUsers={};
+
+io.on('connection', socket=>{
+    const { user_id } = socket.handshake.query;
+    
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) =>{
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
 
 // GET, POST, PUT DELETE
 
@@ -25,4 +46,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
